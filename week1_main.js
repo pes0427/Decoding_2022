18422c83ffc8a486210028854aa68c16b08d9347
pes0427 <pes0427@naver.com>
2022-09-20 20:11:24 +0900

    week1 todo-list code

--- week1_main.js
+++ week1_main.js
@@ -3,7 +3,7 @@ init();//실행
 function init(){
     document.querySelector('form').addEventListener('submit', addTodo);
     document.getElementById('clearAll').addEventListener('click', clearAll);
-    document.querySelector('ul').addEventListener('click', delTodo);
+    document.querySelector('ul').addEventListener('click', delOrDoneTodo);
 }
 
 function addTodo(e){
@@ -17,18 +17,32 @@ function addTodo(e){
     }
 }
 
+function delOrDoneTodo(e){
+    if(e.target.className == 'del'){
+        delTodo(e);
+    } else {
+        doneTodo(e);
+    }
+}
+
 function addTask(value){
     let ul = document.querySelector('ul');
     let li = document.createElement('li');
-    li.innerHTML = `<button class="del">X</button> <label id="val">${value}</label>`;
+    li.innerHTML = `<button class="del">X</button><label id="val">${value}</label><button class="done">DONE</button>`;
     ul.appendChild(li); //리스트 내용 추가
 }
 
 function delTodo(e){
     let remove = e.target.parentNode;
     let parentNode = remove.parentNode;
-    if(e.target.className == 'del')
-        parentNode.removeChild(remove);
+    parentNode.removeChild(remove);
+}
+
+function doneTodo(e){
+    let done = e.target.previousSibling;
+    if(e.target.className == 'done'){
+        done.style.textDecoration = "line-through";
+    }
 }
 
 function clearAll(e){
